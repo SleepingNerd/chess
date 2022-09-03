cf2a6c28126a2efa63de0a439acb501b64e4b457
Sami Arredouani <samiarre08@gmail.com>
2022-09-03 23:18:49 +0200

    First version of texture.py, not tested yet

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ pygame.init()
 
 config_f = open("config.json")
 config = json.load(config_f)
-e = "\assets\texture_packs" + config["texture_pack"]
-print(e)
+config_f.close()
+
+e = "/assets/texture_packs/" + config["texture_pack"]
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
